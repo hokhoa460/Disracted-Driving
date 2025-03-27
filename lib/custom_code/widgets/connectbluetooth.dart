c73de2bb343402d0aeafcdfa9a34c5330382ddd4
hokhoa460 <hokhoa460@gmail.com>
2025-03-27 14:54:43 -0500

    Create connectbluetooth.dart
    Create connectbluetooth.dart widget

---
+++ lib/custom_code/widgets/connectbluetooth.dart
@@ -0,0 +1,73 @@
+// Automatic FlutterFlow imports
+import '/backend/backend.dart';
+import '/backend/schema/structs/index.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom widgets
+import 'package:flutter/material.dart';
+// Begin custom widget code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BluetoothScanPage(),
+    );
+  }
+}
+
+class BluetoothScanPage extends StatefulWidget {
+  @override
+  _BluetoothScanPageState createState() => _BluetoothScanPageState();
+}
+
+class _BluetoothScanPageState extends State<BluetoothScanPage> {
+  FlutterBluePlus flutterBlue = FlutterBluePlus.instance;
+  List<ScanResult> scanResults = [];
+
+  @override
+  void initState() {
+    super.initState();
+    startScan();
+  }
+
+  void startScan() {
+    flutterBlue.startScan(timeout: Duration(seconds: 4));
+
+    flutterBlue.scanResults.listen((results) {
+      setState(() {
+        scanResults = results;
+      });
+    });
+
+    flutterBlue.stopScan();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bluetooth Scan'),
+      ),
+      body: ListView.builder(
+        itemCount: scanResults.length,
+        itemBuilder: (context, index) {
+          ScanResult result = scanResults[index];
+          return ListTile(
+            title: Text(result.device.name.isEmpty
+                ? 'Unknown Device'
+                : result.device.name),
+            subtitle: Text(result.device.id.id),
+          );
+        },
+      ),
+    );
+  }
+}
