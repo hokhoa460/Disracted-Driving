50829abd00b19ab217750fb6df6b04a013478091
hokhoa460 <hokhoa460@gmail.com>
2025-04-07 19:52:18 -0500

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,16 @@
 import UIKit
-
+import FirebaseCore
 import Flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+  var window: UIWindow?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure() // Initialize Firebase
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
